3beaeaba17350d63e025fd5c9ed8e25b056b78f4
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-07 23:47:21 +0600

    search functionality enabled

--- React-Router-V7/REST Countries/app/routes/Countries.tsx
+++ React-Router-V7/REST Countries/app/routes/Countries.tsx
@@ -11,16 +11,29 @@ export async function clientLoader() {
 const Countries = ({ loaderData }: Route.ComponentProps) => {
   const [search, setSearch] = useState<string>("")
   
+  // const filteredCountries = loaderData?.filter((country: any) =>
+  //   country.name.common.toLowerCase().includes(search.toLowerCase())
+  // );
+
+  const filteredCountries = loaderData?.filter((country: any) => {
+    const matchesSearch = !search || country.name.common.toLowerCase().includes(search.toLocaleLowerCase())
+    return matchesSearch
+  })
 
   
   return (
     <div className='px-4 lg:px-8 xl:px-16'>
       <h2 className='text-2xl font-semibold text-center my-4'>Countries</h2>
       <div className='flex items-center justify-center'>
-        <input type='text' placeholder='Search by name...' value={search} onChange={(e) => setSearch(e.target.value)}  className='border px-2 py-1 rounded-md'/>
+        <input
+          type='text'
+          placeholder='Search by name...'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className='border px-4 py-2 rounded-md' />
       </div>
       <div className='grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 lg:gap-6 mt-8'>
-        {loaderData?.map((country: any, key: number) => (
+        {filteredCountries.map((country: any, key: number) => (
           <div key={country.alpha3Code} className=' border p-6 rounded-lg text-center'>
             <Link to={`/countries/${country.name.common}`} className='text-lg font-semibold my-2'>
               {country.name.common}
