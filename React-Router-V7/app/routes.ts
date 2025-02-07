416d5b3617116f08774a5d97b6f3a56f42f7e9ba
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-07 15:54:15 +0600

    loader and client loader explored

--- React-Router-V7/app/routes.ts
+++ React-Router-V7/app/routes.ts
@@ -1,20 +1,29 @@
-import { type RouteConfig, index, layout, route } from '@react-router/dev/routes';
+import { type RouteConfig, index, layout, prefix, route } from '@react-router/dev/routes';
 
 export default [
   index('routes/home.tsx'),
+  ...prefix('ru', [
   route('/about', 'routes/about.tsx'),
-  route('/post/:postId', 'routes/post.tsx'),
+    route('/post/:postId', 'routes/post.tsx'),
+  ]),
 
   // Nested routes
-  // route('dashboard', 'routes/Dashboard.tsx', [
+  route('dashboard', 'routes/Dashboard.tsx', [
+    route('user', 'routes/UserInfo.tsx'),
+    route('finances', 'routes/Finances.tsx'),
+  ])
+
+  // layout
+  // layout('./routes/Dashboard.tsx', [
   //   route('user', 'routes/UserInfo.tsx'),
   //   route('finances', 'routes/Finances.tsx'),
   // ])
 
-  // layout
-  layout('./routes/Dashboard.tsx', [
-    route('user', 'routes/UserInfo.tsx'),
-    route('finances', 'routes/Finances.tsx'),
-  ])
- 
+  // prefix
+  // layout('./routes/Dashboard.tsx', [
+  //   ...prefix('user', [
+  //     route('info', 'routes/UserInfo.tsx'),
+  //     route('finances', 'routes/Finances.tsx'),
+  //   ])
+  // ]),
 ] satisfies RouteConfig;
--- React-Router-V7/app/routes/post.tsx
+++ React-Router-V7/app/routes/post.tsx
@@ -1,14 +1,27 @@
 import type { Route } from "./+types/post";
 
-export async function loader({ params }: Route.LoaderArgs) {
+// client loader
+export async function clientLoader({ params }: Route.LoaderArgs) {
   const postId = params.postId;
-  return {postId}
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  return res.json();
 }
+// loader
+// export async function loader({ params }: Route.LoaderArgs) {
+//   const product = await db.getProduct(params.id)
+// }
 export async function action() {
   
 }
 export default function Post({ loaderData }: Route.ComponentProps) {
-  return <div>
-    <p>Post Id: {loaderData?.postId}</p>
-  </div>
+  return (
+    <div>
+      <h1 className='text-2xl font-semibold my-4 text-center underline'>
+        <span className="font-bold italic mr-2 text-amber-500"> Title:</span> {loaderData.title}
+      </h1>
+      <p className='border border-amber-400 mx-2 p-4 rounded-2xl'>
+        Body: {loaderData.body}
+      </p>
+    </div>
+  );
 }
