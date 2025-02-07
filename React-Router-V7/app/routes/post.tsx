8518244bc020be883282439ed4714c5ea28d9ca0
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-07 16:39:41 +0600

    data fetching and delete method explored

--- React-Router-V7/app/routes/post.tsx
+++ React-Router-V7/app/routes/post.tsx
@@ -1,3 +1,4 @@
+import { Form, redirect, useFetcher, useNavigate } from "react-router";
 import type { Route } from "./+types/post";
 
 // client loader
@@ -10,18 +11,49 @@ export async function clientLoader({ params }: Route.LoaderArgs) {
 // export async function loader({ params }: Route.LoaderArgs) {
 //   const product = await db.getProduct(params.id)
 // }
-export async function action() {
-  
+export async function clientAction({params}: Route.ClientActionArgs) {
+ try {
+   await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`, {
+     method: 'DELETE',
+   });
+   return { isDeleted: true };
+ } catch (error) {
+    return { isDeleted: false };
+ }
 }
 export default function Post({ loaderData }: Route.ComponentProps) {
+  const fetcher = useFetcher();
+  const navigate = useNavigate()
+  
+  const isDeleted = fetcher.data?.isDeleted;
   return (
     <div>
-      <h1 className='text-2xl font-semibold my-4 text-center underline'>
-        <span className="font-bold italic mr-2 text-amber-500"> Title:</span> {loaderData.title}
-      </h1>
-      <p className='border border-amber-400 mx-2 p-4 rounded-2xl'>
-        Body: {loaderData.body}
-      </p>
+      {isDeleted ? (
+        <div>
+          <h1 className='text-2xl font-semibold my-4 text-center underline'>
+            <span className='font-bold italic mr-2 text-amber-500'>
+              {' '}
+              Title:
+            </span>{' '}
+            {loaderData.title}
+          </h1>
+          <p className='border border-amber-400 mx-2 p-4 rounded-2xl'>
+            Body: {loaderData.body}
+          </p>
+        </div>
+      ) : 'Data Deleted Successfully'}
+
+      <div>
+      <button className="bg-rose-700 px-6 py-2 rounded-md" onClick={() => navigate('/')}>Redirect</button>
+      </div>
+
+      <div className='w-1/2 mx-auto my-8'>
+        <fetcher.Form method='delete'>
+          <button type='submit' className='px-6 py-2 bg-orange-700 rounded-lg'>
+            Delete
+          </button>
+        </fetcher.Form>
+      </div>
     </div>
   );
 }
